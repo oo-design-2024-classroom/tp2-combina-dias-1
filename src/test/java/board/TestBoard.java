01d1b4585d625fd7779a7a8b4063fe8ee7c2d088
Ignacio Dias Gundin <ignadiasg@gmail.com>
2024-05-15 23:31:04 -0300

    tests :D

--- src/test/java/board/TestBoard.java
+++ src/test/java/board/TestBoard.java
@@ -27,13 +27,25 @@ public class TestBoard {
             }
         }
     }
-
     @Test
     public void addCellToBoard(){
         board = new Board(2,2,new Cell(CellType.DEAD), new ArrayList<Rule>());
         board.setCell(0,1,new Cell(CellType.ALIVE));
         cell = board.getCell(0,1);
         assertTrue(cell.isAlive());
+    }
+    @Test
+    public void testNoNeighbours() {
+        board = new Board(3, 3, new Cell(CellType.DEAD), new ArrayList<Rule>());
+        assertThat(board.getNeighbors(1,1)).isEqualTo(0);
+    }
+    @Test
+    public void testNeighbours() {
+        board = new Board(3, 3, new Cell(CellType.DEAD), new ArrayList<Rule>());
+        board.setCell(0,0,new Cell(CellType.ALIVE));
+        board.setCell(0,1,new Cell(CellType.ALIVE));
+        board.setCell(2,2,new Cell(CellType.ALIVE));
+        assertThat(board.getNeighbors(1,1)).isEqualTo(3);
     }
     @Test
     public void checkGeneration() {
@@ -55,5 +67,20 @@ public class TestBoard {
         expected.setCell(2,3,new Cell(CellType.ALIVE));
 
         assertThat(newBoard).isEqualTo(expected);
+    }
+    @Test
+    public void testGenerationCycle() {
+        List<Rule> rules = new ArrayList<>();
+        rules.add(new RuleBorn());
+        rules.add(new RuleDieOverpopulation());
+        rules.add(new RuleDieUnderpopulation());
+        rules.add(new RuleStayAlive());
+        board = new Board(5,5,new Cell(CellType.DEAD), rules);
+        board.setCell(1,2,new Cell(CellType.ALIVE));
+        board.setCell(2,2,new Cell(CellType.ALIVE));
+        board.setCell(3,2,new Cell(CellType.ALIVE));
+        Board newBoard = board.nextGeneration();
+        newBoard = newBoard.nextGeneration();
+        assertThat(newBoard).isEqualTo(board);
     }
 }
